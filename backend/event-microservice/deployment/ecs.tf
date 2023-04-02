4767e0c1a57f0456022bfd51272ab446cc907b07
Stedra Kristóf <stedra99@gmail.com>
2023-04-02 14:59:49 +0200

    Add test fargate deployment

---
+++ backend/event-microservice/deployment/ecs.tf
@@ -0,0 +1,47 @@
+resource "aws_ecs_cluster" "main" {
+  name = "cb-cluster"
+}
+
+data "template_file" "cb_app" {
+  template = file("./template/cb_app.json.tpl")
+
+  vars = {
+    app_image      = "public.ecr.aws/s8d2y0e0/eventmanagerservice:latest"
+    app_port       = 80
+    fargate_cpu    = 1024
+    fargate_memory = 2048
+    aws_region     = var.region
+  }
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "cb-app-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = data.template_file.cb_app.rendered
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "cb-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.id
+    container_name   = "cb-app"
+    container_port   = 80
+  }
+
+  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+}
