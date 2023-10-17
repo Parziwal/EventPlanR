d1c1cc81d535bfae3fa27243c5a406b83fdf6e9a
Parziwal <stedra99@gmail.com>
2023-10-17 08:54:45 +0200

    Wire in all lambda functions to the api gateway

--- infrastructure/backend/development/api_lambda_services.tf
+++ infrastructure/backend/development/api_lambda_services.tf
@@ -2,12 +2,12 @@ locals {
   lambda_source_dir      = "../../../backend/Serverless/{LAMBDA_FOLDER}/bin/Release/net6.0/publish"
   aspnetcore_environmnet = "Development"
   lambdas = {
-    //event_general_api = "EventPlanr.EventGeneral.Api",
-    //event_manager_api = "EventPlanr.EventManager.Api",
+    event_general_api = "EventPlanr.EventGeneral.Api",
+    event_manager_api = "EventPlanr.EventManager.Api",
     organization_manager_api = "EventPlanr.OrganizationManager.Api",
-    //ticket_manager_api = "EventPlanr.TicketManager.Api",
-    //ticket_order_api = "EventPlanr.TicketOrder.Api",
-    //user_ticket_api = "EventPlanr.UserTicket.Api"
+    ticket_manager_api = "EventPlanr.TicketManager.Api",
+    ticket_order_api = "EventPlanr.TicketOrder.Api",
+    user_ticket_api = "EventPlanr.UserTicket.Api"
   }
 }
 
--- infrastructure/backend/development/auth.tf
+++ infrastructure/backend/development/auth.tf
@@ -18,11 +18,17 @@ module "pre_token_generation_lambda" {
   }
 }
 
-resource "aws_lambda_permission" "this" {
-  statement_id  = "allow_execution_from_conito"
+resource "aws_lambda_permission" "allow_execution_from_cognito" {
+  statement_id  = "allow_execution_from_cognito"
   action        = "lambda:InvokeFunction"
   function_name = module.pre_token_generation_lambda.function_name
   principal     = "cognito-idp.amazonaws.com"
 
   source_arn = module.event_planr_auth.user_pool_arn
+}
+
+resource "aws_ssm_parameter" "cognito_user_pool_id" {
+  name        = "/${var.environment}/event_planr/CognitoUserPoolOptions/UserPoolId"
+  type        = "String"
+  value       = module.event_planr_auth.user_pool_id
 }
